15e1367e357e5c8e770e67b4ddd0b3a1656f45cd
Hamza Hamid Khan <109942374+ihamzahkgithub@users.noreply.github.com>
2023-12-11 09:47:19 +0500

    Task 3 CodSoft

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: avoid_print, prefer_const_constructors
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'home_screen.dart';
+import 'views/sign_in_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCKAWa40eJho6Z25Ad0lGV6lzsqNF1eNW0",
+      appId: "1:729142601350:android:14abd8858e301881a3d307",
+      messagingSenderId: "729142601350",
+      projectId: "my-note-a66f2",
+    ),
+  );
+  await Firebase.initializeApp();
+
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  User? user;
+
+  @override
+  void initState() {
+    super.initState();
+    user = FirebaseAuth.instance.currentUser;
+    print(user?.uid.toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: user != null ? const HomeScreen() : const LoginScreen(),
+    );
+  }
+}
